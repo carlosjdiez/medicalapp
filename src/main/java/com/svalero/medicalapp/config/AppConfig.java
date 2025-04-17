19239c46df31d8304e8e82b610c2cd21807d5aeb
carlosjdiez <jdiezcarlos@gmail.com>
2025-04-17 17:31:55 +0200

    Agregacion conexion a la base de datos a traves de application.properties en resources

--- src/main/java/com/svalero/medicalapp/config/AppConfig.java
+++ src/main/java/com/svalero/medicalapp/config/AppConfig.java
@@ -2,7 +2,10 @@ package com.svalero.medicalapp.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
     @Bean
